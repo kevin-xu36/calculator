de2f4b47dc63101f6c7cb5ff26ccd74151e7bb86
kevin-xu36 <176247388+kevin-xu36@users.noreply.github.com>
2025-04-23 02:13:47 -0400

    Implemented basic calculator UI and a few calculator functions

---
+++ calculatorJs.js
@@ -0,0 +1,34 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+function operate(num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else {
+        return divide(num1, num2);
+    }
+}
+
+let number1 = 5;
+let number2 = 2;
+let operator = '/';
+
+console.log(operate(number1, number2, operator));
+
